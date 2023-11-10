5493d8e43fa938fb0fd7810716cd85bfad322412
mwaidesu <machariamwai@gmail.com>
2023-11-10 15:06:34 +0300

    Add search bar and listview placeholder code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,25 +20,24 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.transparent,
         leading: Icon(Icons.menu),
         actions: [
-          Padding(padding: EdgeInsets.only(right: 25.0,
-          ),
-          child: Icon(Icons.person),
+          Padding(
+            padding: EdgeInsets.only(
+              right: 25.0,
+            ),
+            child: Icon(Icons.person),
           ),
         ],
       ),
-      bottomNavigationBar: 
-      BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: "",
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.favorite),
             label: "",
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.notifications),
             label: "",
@@ -47,22 +46,72 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-              //best coffee title
-          Padding(padding: EdgeInsets.symmetric(horizontal: 25.0),
-          child: Text("F I N D   T H E  B E S T  C O F F E E  E V E R",
-          style: GoogleFonts.bebasNeue(
-            fontSize: 56,
-
+          //best coffee title
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              "F I N D   T H E  B E S T  C O F F E E    E V E R !",
+              style: GoogleFonts.bebasNeue(
+                fontSize: 56,
+              ),
+            ),
           ),
-          ),),
-          SizedBox(height: 25,),
-      
-
-
-
+          SizedBox(
+            height: 25,
+          ),
           //searchbar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: "Find Your Cup of Coffee...",
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+              ),
+            ),
+          ),
 
+          SizedBox(
+            height: 25.0,
+          ),
           //horizontal listview
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
 
           //bottom nav bar
         ],
